708e5f8155d99c0dd40b7bf5d0f8dd785ef498d9
Federico Croce <federico.croce@bbva.com>
2018-03-21 20:37:26 -0300

    Implementacion de AMP con react

--- src/Components/Sections/App.js
+++ src/Components/Sections/App.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import logo from './fondo.jpg';
+
 // import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <h1 className="App-title">Welcome to React Fede</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Welcome to the mobile web</h1>
+        {/*<amp-img src={logo} layout="flex-item" width="266" height="150"></amp-img>
+        <section id="bloque1">
+          <amp-img src={logo} layout="flex-item" width="266" height="150"></amp-img>
+          <h1>SOY FEDE</h1>
+        </section>
+        <section id="bloque2">
+          <amp-img src={logo} layout="flex-item" width="266" height="150"></amp-img>
+          <h1>SOY FEDE</h1>
+        </section>
+        <section id="bloque3">
+          <amp-img src={logo} layout="responsive" width="266" height="150"></amp-img>
+          <h1>SOY FEDE</h1>
+        </section>*/}
+        <amp-img src={logo} layout="flex-item" width="266" height="150"></amp-img>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis venenatis, metus a tristique aliquet, enim nibh efficitur sem, ut iaculis urna justo eu diam. Nullam cursus sapien et sodales posuere. Vestibulum ante ipsum primis in faucibus orci luctus et
+  ultrices posuere cubilia Curae; Suspendisse potenti. Donec vitae ornare risus. Maecenas eleifend ante vel dui laoreet, et porttitor libero rutrum. Nam arcu mi, ullamcorper at risus et, pulvinar ultrices erat. In pellentesque sem vel purus auctor,
+  ut venenatis tellus tristique. Phasellus molestie diam orci, nec gravida turpis bibendum ut. Sed sagittis aliquet lorem sed dictum.
+</p>
+        <amp-fx-flying-carpet height="300px">
+          <amp-img src={logo} layout="flex-item" width="3000" height="3000"></amp-img>
+        </amp-fx-flying-carpet>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis venenatis, metus a tristique aliquet, enim nibh efficitur sem, ut iaculis urna justo eu diam. Nullam cursus sapien et sodales posuere. Vestibulum ante ipsum primis in faucibus orci luctus et
+  ultrices posuere cubilia Curae; Suspendisse potenti. Donec vitae ornare risus. Maecenas eleifend ante vel dui laoreet, et porttitor libero rutrum. Nam arcu mi, ullamcorper at risus et, pulvinar ultrices erat. In pellentesque sem vel purus auctor,
+  ut venenatis tellus tristique. Phasellus molestie diam orci, nec gravida turpis bibendum ut. Sed sagittis aliquet lorem sed dictum.
+</p>
+{/*<amp-img src={logo} layout="responsive" width="266" height="150"></amp-img>
+<amp-img src={logo} layout="responsive" width="266" height="150"></amp-img>
+<amp-img src={logo} layout="responsive" width="266" height="150"></amp-img>
+<amp-img src={logo} layout="responsive" width="266" height="150"></amp-img>*/}
+
       </div>
     );
   }
 }
 
 export default App;
+
+
+
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import ReactDOM from 'react-dom';
 import './Styles/Main/index.css';
 import frameworkConfig from './Config/frameworkConfig';
 import Index from './Components/Sections/Index';
-import App from './Components/Sections/App';
+import AppAMP from './Components/Sections/App';
 
 import registerServiceWorker from './registerServiceWorker';
 
 frameworkConfig();
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(<AppAMP />, document.getElementById('root'));
 registerServiceWorker();
